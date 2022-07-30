592da53a54b3ea447ec7d7e71a1fa8659fd97edd
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2022-07-30 14:38:26 -0500

    removed import

--- src/constants/addCantoToWallet.ts
+++ src/constants/addCantoToWallet.ts
@@ -1,6 +1,5 @@
 import { ethers } from "ethers";
 import { CantoMain } from "constants/networks";
-import { useNetworkInfo } from "stores/networkInfo";
 
 export function addNetwork() {
   //@ts-ignore
