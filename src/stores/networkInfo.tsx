7850f24021428512afbf5aadc45276d61daa95c8
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2022-10-17 12:06:16 -0400

    removed auto switch

--- src/stores/networkInfo.tsx
+++ src/stores/networkInfo.tsx
@@ -29,7 +29,6 @@ export const useNetworkInfo = create<NetworkProps>()(
         set({ isConnected: true });
       } else {
         set({ isConnected: false });
-        addNetwork();
       }
     },
     account: undefined,
